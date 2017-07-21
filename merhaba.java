568dc679bb38c3e93688e407b74d45bee4f2e2f3
irhallac <ibrahimrizahallac@live.com>
2017-07-22 00:27:50 +0300

    Add files via upload

---
+++ merhaba.java
@@ -0,0 +1,20 @@
+/**
+ *
+ * @author sentis
+ */
+
+public class merhaba {
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Merhaba kullanici");
+        } else if (args.length == 1) {
+            System.out.println("Merhaba " + args[0]);
+        } else if (args.length == 2) {
+            System.out.println("Merhaba " + args[0] + " ve " + args[1]);
+        }
+        else{
+            System.out.println("2'den fazla kullanici girilemez");
+        }
+    }
+}
