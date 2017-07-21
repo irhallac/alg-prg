bcbcebfb484a447cb2d003de004b362df5ae6c46
irhallac <ibrahimrizahallac@live.com>
2017-07-22 00:57:01 +0300

    Add files via upload

---
+++ algoritma2/istisna.java
@@ -0,0 +1,22 @@
+package labexercises;
+
+/**
+ *
+ * @author Ibrahim R Hallac <irhallac at firat.edu.tr>
+ */
+public class istisna {
+
+    public static void main(String[] args) {
+        int a, b, t;
+        a = 4;
+        b = 0;
+        try {
+            t = a / b;
+            System.out.println("t=" + t);
+
+        } catch (Exception e) {
+            System.out.println("hata olustu orjinal hata mesajı asagida");
+            System.out.println(e.getMessage());
+        }
+    }
+}
