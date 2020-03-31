5da32bc960922421be537cbd3b2545b768b4e02c
irhallac <ibrahimrizahallac@live.com>
2020-03-31 21:14:27 +0300

    Create bolum2.java

---
+++ algoritma2/java-static/bolum2.java
@@ -0,0 +1,22 @@
+//Adım-1
+public class Musteri {
+    String isim;
+    int yas;
+    int m_no;
+    double yuzde_indirim;
+    
+    public void bilgileriYazdir(){
+            //....
+    }  
+}
+
+//Adım-2
+
+
+
+
+//Adım-3
+
+
+
+//Adım-4
